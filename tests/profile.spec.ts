bcbdd5c10aaa307ca55eac72095995d3e4401179
sriveros21 <s.riverosg@uniandes.edu.co>
2022-11-11 01:08:22 -0800

    Edit profile

---
+++ tests/profile.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.serial("Post E2E Scenarios", () => {
+    test.beforeEach(async ({ page }) => {
+      // Go to the starting url before each test.
+      await page.goto("");
+  
+      // Expect a title "to contain" a substring.
+      await expect(page).toHaveTitle(/Sign In - HyperS/);
+  
+      // login in the admin panel
+      await page.locator('id=ember6').fill('sr@34.com');
+      await page.locator('id=ember8').fill('3102326449');
+      await page.getByText('Sign in →').click();
+  
+      // Expects the URL to contain dashboard.
+      await expect(page).toHaveURL(/.*dashboard/);
+    });
+  
+    test('Edit profile', async ({ page }) => {
+  
+      await page.goto('/ghost/#/settings/staff/sergio');
+    
+      // Expects to be in the staff section
+      await expect(page).toHaveTitle(/Staff - User - HyperS/);
+    
+      // Creation of post
+      await page.locator('id=user-name').fill('Sergio Andres');
+      await page.locator('id=user-slug').fill('Sriveros7');
+      await page.locator('id=user-location').fill('Colombia');
+      await page.locator('id=user-website').fill('www.mokanacolombia.com');
+      await page.locator('id=user-bio').fill('Me gustan los deportes extremos y viajar :D ');
+      await page.getByText('Save').click();
+
+      await page.goto('/author/sriveros7/');
+      await expect(page).toHaveTitle(/Sergio Andres - HyperS/);
+
+      await page.goto('/ghost/#/settings/staff/sriveros7');
+      await page.locator('id=user-slug').fill('Sergio');
+      await page.getByText('Save').click();
+    });
+
+    test.afterAll(async ({ page }) => {
+        page.close();
+      });
+});
