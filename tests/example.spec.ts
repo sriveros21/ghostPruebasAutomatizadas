84abd3be270f02d2e925b62bce5ea25a6fb8650d
sriveros21 <s.riverosg@uniandes.edu.co>
2022-11-10 20:32:26 -0800

    Create member

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Create a new member', async ({ page }) => {
+  await page.goto('');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Sign In - HyperS/);
 
   // create a locator
-  const getStarted = page.getByRole('link', { name: 'Get started' });
+  await page.locator('id=ember6').fill('sr@34.com');
+  await page.locator('id=ember8').fill('3102326449');
+  await page.getByText('Sign in →').click();
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
-  // Click the get started link.
-  await getStarted.click();
+  // Expects the URL to contain dashboard.
+  await expect(page).toHaveURL(/.*dashboard/);
+  await page.goto('/ghost/#/members');
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // Expects to be in the creation of member section
+  await expect(page).toHaveTitle(/Members - HyperS/);
+  await page.getByText('New member').click();
+  const breadcrumb = await page.locator('.gh-canvas-breadcrumb').textContent();
+  expect (breadcrumb).toContain("New member");
+
+  await page.locator('id=member-name').fill('Sergio');
+  await page.locator('id=member-email').fill('ar32@gmail.com');
+  await page.locator('.ember-power-select-trigger-multiple-input').fill('new');
+  const newsletterToggle = page.locator('.input-toggle-component');
+  if (await newsletterToggle.isChecked() != true)
+    newsletterToggle.check();
+  await page.getByText('Save').click();
+  expect(await page.locator('.gh-member-details-identity').textContent()).toContain('ar32@gmail.com');
 });
